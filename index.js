2442d08a2e66bb7deda103545290370f99a5b31d
Amirulmath <amirulmath760@gmail.com>
2022-10-27 10:36:08 +0600

    Cors install & course lists API send

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courseLists = require('./data/courseLists.json');
 
 app.get('/', (req, res) =>{
     res.send('Course API Running');
 });
+
+app.get('/course-lists', (req, res) =>{
+    res.send(courseLists)
+})
 
 app.listen(port, () =>{
     console.log('Learning Computer Server running', port);
