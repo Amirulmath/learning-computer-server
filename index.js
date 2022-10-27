d353046f75b2d3c8a1d23004b8113eb18a62a41a
Amirulmath <amirulmath760@gmail.com>
2022-10-27 14:00:32 +0600

    Course API send to client

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const courseLists = require('./data/courseLists.json');
+const courses = require('./data/course.json');
 
 app.get('/', (req, res) =>{
     res.send('Course API Running');
@@ -13,6 +14,18 @@ app.get('/', (req, res) =>{
 
 app.get('/course-lists', (req, res) =>{
     res.send(courseLists)
+});
+
+app.get('/list/:id', (req, res) =>{
+    const id =req.params.id;
+    const lists = courses.filter(course => course._id ===id);
+    res.send(lists);
+})
+
+app.get('/course/:id', (req, res) =>{
+    const id = req.params.id;
+    const selecteCourse = courses.find(course => course._id === id);
+    res.send(selecteCourse);
 })
 
 app.listen(port, () =>{
